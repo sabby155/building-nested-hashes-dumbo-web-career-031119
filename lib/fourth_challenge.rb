720338ec7cd1577d126679a2aa812f3f619cfb32
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-14 20:38:06 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,7 +24,7 @@ def fourth_challenge
       :heroine_friends => [
           { name: "Steven" , 
           age: "30" ,
-          attitude: "worried"
+          attitude: "confused"
           
         },
         {
