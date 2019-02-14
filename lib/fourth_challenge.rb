b66c30fcf11207c38c07a9fb1ee3f6085d0233e0
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-14 20:36:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,11 +5,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {},
+        { name: "Benvolio" , 
+          age: "17" ,
+          attitude: "worried"},
         {
           name: "Benvolio" , 
           age: "17" ,
-          attitude: 
+          attitude: "worried"
         }
       ]
    }, 
